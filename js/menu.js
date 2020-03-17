78b30228963f228d23244f22c510f15456068215
Raul Moreno <raulmoreno899@gmail.com>
2020-03-17 00:32:57 -0600

    only left footer

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+$("#home").click(() => {
+    window.location.href = "index.html"
+});
+$("#about-us").click(() => {
+    window.location.href = "about-us.html"
+});
+$("#services").click(() => {
+    window.location.href = "services.html"
+});
